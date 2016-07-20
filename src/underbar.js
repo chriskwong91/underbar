2aa74d9bf5130ac05e2d02b3a6b5221a320e44df
Christopher Wong <chriskwong91@gmail.com>
2016-07-19 22:18:00 -0700

    completed up to "reject"

--- src/underbar.js
+++ src/underbar.js
@@ -93,6 +93,14 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var rejected = [];
+    var filtered = _.filter(collection,test);
+    _.each(collection, function(item) {
+      if (!filtered.includes(item)){
+        rejected.push(item);
+      }
+    });
+    return rejected;
   };
 
   // Produce a duplicate-free version of the array.
